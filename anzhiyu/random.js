9a5ff20e4312ddcb8877a68a01fa3ed0c29379e7
protme <liam99ok66@gmail.com>
2025-07-05 08:51:18 +0800

    Site updated: 2025-07-05 08:51:17

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/07/04/诗经·桃夭/","2025/07/04/诗经-关雎/","2025/07/04/hello-world/"];function toRandomPost(){
+var posts=["2025/07/05/蝶恋花-庭院深深深几许/","2025/07/04/诗经·桃夭/","2025/07/04/诗经-关雎/","2025/07/04/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
